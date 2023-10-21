0c610cd56da2ec14de415868d438d333d1d885b2
Shashvat Tiwari <105010005+shashvat3107@users.noreply.github.com>
2023-10-21 13:15:01 +0530

    final code
    Ecommerce MERN Stack App

---
+++ routes/categoryRoutes.js
@@ -0,0 +1,43 @@
+import  express  from "express";
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
+import {    
+            categoryController,
+            createCategoryController, 
+            deleteCategoryController, 
+            singleCategoryController, 
+            updateCategoryController 
+        } from "../controllers/categoryController.js";
+
+const router = express.Router()
+
+//routes
+// create Category
+router.post(
+    '/create-category' ,
+    requireSignIn , 
+    isAdmin , 
+    createCategoryController);
+
+//update Category
+router.put(
+    '/update-category/:id' , 
+    requireSignIn , 
+    isAdmin , 
+    updateCategoryController );
+
+//getAll Category
+router.get('/get-category', categoryController)
+
+
+
+// get a  single category
+router.get('/single-category/:slug' , singleCategoryController)
+
+//delete category
+router.delete(
+    '/delete-category/:id' , 
+    requireSignIn , 
+    isAdmin , 
+    deleteCategoryController
+    );
+export default router;
